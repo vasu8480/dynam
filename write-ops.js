1e3b853e310621ced5dcc680b51d390fe2dc463e
vasu8480 <vasu8480@gmail.com>
2022-09-14 11:29:47 +0530

    write-ops

---
+++ write-ops.js
@@ -0,0 +1,74 @@
+const AWS= require('aws-sdk');		/// initiatlly we create a databse table with name vasu_dynamo
+AWS.config.update({region: 'us-east-1'});
+
+const docclient = new AWS.DynamoDB.DocumentClient();
+
+
+docclient.put({
+						TableName:"vasu-sdk",
+						Item:{
+							id:5,
+							name:"dfas",
+							age:22
+						}}
+						,(err,data)=>{
+		if (err) console.log(err, err.stack); // an error occurred
+		else     console.log(data);           // successful response
+		}
+		);
+
+docclient.update({
+						TableName:"vasu-sdk",	
+						Key:{
+							id:1
+						},
+						UpdateExpression:"set #n=:n",
+						ExpressionAttributeNames:{
+							"#n":"name"
+						},
+						ExpressionAttributeValues:{
+							":n":"vasu"
+						},
+						}
+						,(err,data)=>{
+		if (err) console.log(err, err.stack); // an error occurred
+		else     console.log(data);           // successful response
+		}
+		);
+
+// docclient.delete({
+// 							TableName:"vasu-sdk",
+// 							Key:{
+// 								id:1
+// 							}
+// 						}
+// 						,(err,data)=>{
+// 		if (err) console.log(err, err.stack); // an error occurred
+// 		else     console.log(data);           // successful response
+// 		}
+// 		);
+
+docclient.batchWrite({
+		RequestItems:{
+			"vasu-sdk":[
+				{
+					DeleteRequest:{
+						Key:{
+							id:5
+						}
+					}
+				},
+				{
+					PutRequest:{
+						Item:{
+							id:2,
+							name:"vasu",
+							age:22
+						}
+					}
+				}]
+		}
+	},(err,data)=>{
+			if (err) console.log(err, err.stack); // an error occurred
+			else     console.log(data);           // successful response
+	});
